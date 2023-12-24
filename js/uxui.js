fbb154604dda893e9f0a563d6804a2ac3e6bb5ee
José Vitor Tostes De Andrade <153818181+jose-jv@users.noreply.github.com>
2023-12-24 06:54:45 -0300

    site Jv
    Site JV

---
+++ js/uxui.js
@@ -0,0 +1,1 @@
+document.getElementById("PowerUpbutton").addEventListener("click",function(){window.location.href="powerup.html"}),document.getElementById("GameOnButton").addEventListener("click",function(){window.location.href="gameon.html"}),document.getElementById("adotEAameButton").addEventListener("click",function(){window.location.href="adoteeame.html"});
